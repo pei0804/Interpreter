b215039de3718f882de708cb1e9e497f606e053f
pei0804 <peeeei0804@gmail.com>
2017-12-06 17:13:18 +0900

    add 1-3

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,9 +1,12 @@
+package lexer
+
+// lexer
 // ASCII 文字のみ対応
 // 様々な文字列に対応する場合は、rune型を使う必要がある
-package lexer
 
 import "github.com/pei0804/interpreter/token"
 
+// Lexer 文字列を解析し、トークン解釈する
 type Lexer struct {
 	// 入力された文字列
 	input string
@@ -16,6 +19,7 @@ type Lexer struct {
 	ch           byte
 }
 
+// New コンストラクタ
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
@@ -30,11 +34,14 @@ func (l *Lexer) readChar() {
 		l.ch = l.input[l.readPosition]
 	}
 	l.position = l.readPosition
-	l.readPosition += 1
+	l.readPosition++
 }
 
+// NextToken 現在の位置にある文字をトークンに変換する 空白は無視する
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -56,9 +63,52 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// ここで許可したものは、文字として扱える
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
--- token/token.go
+++ token/token.go
@@ -1,34 +1,59 @@
 package token
 
 const (
+	// ILLEGAL 何にも識別されなかった文字列
 	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF"
+	// EOF 文字列の終了
+	EOF = "EOF"
 
-	// Identifiers + literals
+	// IDENT 変数名
 	IDENT = "IDENT"
-	INT   = "INT"
+	// INT integer
+	INT = "INT"
 
-	// Oprators
+	// ASSIGN 代入
 	ASSIGN = "="
-	PLUS   = "+"
+	// PLUS ＋演算子
+	PLUS = "+"
 
-	// Delimiters
-	COMMA     = ","
+	// COMMA コンマ
+	COMMA = ","
+	// SEMICOLON コンマ
 	SEMICOLON = ";"
 
+	// LPAREN (
 	LPAREN = "("
+	// RPAREN )
 	RPAREN = ")"
+	// LBRACE {
 	LBRACE = "{"
+	// RBRACE }
 	RBRACE = "}"
 
-	// Keywords
+	// FUNCTION 関数
 	FUNCTION = "FUNCTION"
-	LET      = "LET"
+	// LET 変数宣言
+	LET = "LET"
 )
 
+// TokenType 文字列 Asciiのみ対応想定
 type TokenType string
 
+// Token 文字列情報
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent 特別な意味をもった文字列かチェックする
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
