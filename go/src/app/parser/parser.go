e3822b45a669ce343ddb6223fff93452f560f0fa
pei0804 <peeeei0804@gmail.com>
2017-12-09 14:26:13 +0900

    add 2-5

--- go/src/app/parser/parser.go
+++ go/src/app/parser/parser.go
@@ -70,6 +70,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -98,6 +100,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		p.nextToken()
 	}
 
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+
+	// TODO 型チェックは行わない
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
 	return stmt
 }
 
